b5858d4cde191cda1235926ec5181eec8f80d5a0
Russell Stoneback <rstoneba@utdallas.edu>
2022-03-04 07:28:34 -0600

    STY: Added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020, Authors
+# Full license can be found in License.md and AUTHORS.md
+# -----------------------------------------------------------------------------
+"""Setup routines for pysatTutorials.
+
+Note
+----
+Package metadata stored in setup.cfg
+
+"""
+
+from setuptools import setup
+
+# Run setup
+setup()
